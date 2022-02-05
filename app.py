c48e3aec94427a728c4af293ef5cc2a7fc9332d4
maxidiazbattan <85640305+maxidiazbattan@users.noreply.github.com>
2022-02-05 09:05:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,21 +45,13 @@ data = load_data()
 select_continent = {x : x for x in data["continent"].dropna().unique()}
 
 
-# external_stylesheets = [
-#     {
-#         "href": "https://fonts.googleapis.com/css2?"
-#                 "family=Lato:wght@400;700&display=swap",
-#         "rel": "stylesheet",
-#     },
-# ]
-
 app = dash.Dash(__name__,external_stylesheets = [dbc.themes.CYBORG])# [dbc.themes.CYBORG]) 
 server = app.server
 
 app.layout = dbc.Container([
     
     dbc.Row(dbc.Col(html.P(children="💉", className="header-emoji text-center"), width=12)),
-    dbc.Row(dbc.Col(html.H1("COVID-19 evolution", className='header-title text-center'), width=12)),
+    dbc.Row(dbc.Col(html.H1("COVID-19 Dashboard", className='header-title text-center'), width=12)),
     
     dbc.Row([
             dbc.Col([
@@ -73,35 +65,20 @@ app.layout = dbc.Container([
                      ], width={'size':4, 'offset':4}, className="mb-4"),
             ]), 
              
- 
-    # dbc.Row([    
-    #         dbc.Col([dbc.Card([dbc.CardBody([dcc.DatePickerSingle(id='my-date-picker-start',
-    #                                                               date=data['date'].min(),
-    #                                                               className='p-4'
-    #                                                               ),
-    #                                         dcc.DatePickerSingle(id='my-date-picker-end',
-    #                                                              date=data['date'].max(),
-    #                                                              className='p-4'
-    #                                                              ),])
-    #                         ],),
-    #                     ],width={'size':4, 'offset':4}, className="mt-2"),                  
-    #         ]),
-
-
-    dbc.Row([dbc.Col([dbc.Card([dbc.CardBody([html.Span("Confirmed cases per million", className="card-text"),
+     dbc.Row([dbc.Col([dbc.Card([dbc.CardBody([html.Span("Confirmed cases per million", className="card-text text-center"),
                                              html.H3(style={"color": "#389fd6"}, id="casos-confirmados-text"),
-                                             html.H5(id="nuevos-casos-text", children="0"), ])
-                                 ], className="card" )], width=4),
+                                             html.H5(id="nuevos-casos-text", children="0"),])
+                                 ], className="card text-center" )], width=4),
 
-            dbc.Col([dbc.Card([dbc.CardBody([html.Span("Confirmed deaths per million", className="card-text"),
+            dbc.Col([dbc.Card([dbc.CardBody([html.Span("Confirmed deaths per million", className="card-text text-center"),
                                              html.H3(style={"color": "#data2935"}, id="muertes-confirmadas-text"),
                                              html.H5(id="muertes-text", children="0"),])
-                                ],  className="card" )], width=4),
+                                ],  className="card text-center" )], width=4),
              
-            dbc.Col([dbc.Card([dbc.CardBody([html.Span("Mortality rate %", className="card-text"),
+            dbc.Col([dbc.Card([dbc.CardBody([html.Span("Mortality rate %", className="card-text text-center"),
                                                html.H3(style={"color": "#adatac92"}, id="mortality-rate-text"),
                                                html.H5(id="mortalidad-text", children="0"), ])
-                                 ], className="card" )],width=4),
+                                 ], className="card text-center" )],width=4),
              
              
             ]),
@@ -111,7 +88,7 @@ app.layout = dbc.Container([
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
-                    dcc.Graph(id='area-plot', figure={}),
+                    dcc.Graph(id='pie-plot', figure={}),
                  ]),
              ], ),
          ], width=4, className="mt-2"),
@@ -123,7 +100,7 @@ app.layout = dbc.Container([
                 ]),
             ], ),
         ], width=4, className="mt-2"),
-        
+    
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
@@ -132,6 +109,17 @@ app.layout = dbc.Container([
             ], ),
         ], width=4, className="mt-2"),
     ],),
+
+    dbc.Row([
+            dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='line-plot', figure={})
+                ]),
+            ], ),
+        ], width=12, className="mt-2"),
+    ],),
+    
     
 ], fluid=True)
 
@@ -151,6 +139,9 @@ app.layout = dbc.Container([
 def display_status(continent):#, start_date, end_date):
     
     dfq = data.copy()
+
+    dfqw = dfq.query(f'continent == "{continent}"')
+    casos_acumulados_ww = dfqw['new_cases_per_million'].sum().round(2)
     
     start_date = dfq['date'].min()
     end_date = dfq['date'].max()
@@ -171,27 +162,28 @@ def display_status(continent):#, start_date, end_date):
     
 # Pie Chart ***********************************************************
 @app.callback(
-    Output('area-plot','figure'),
+    Output('pie-plot','figure'),
     Input('continent-dropdown','value'),
     #Input('my-date-picker-start','date'),
     #Input('my-date-picker-end','date'),
 )
-def update_area(continent):#, start_date, end_date):
+def update_pie(continent):#, start_date, end_date):
     
-    dff = data.copy()
+    df1 = data.copy()
 
-    start_date = dff['date'].min()
-    end_date = dff['date'].max()
+    start_date = df1['date'].min()
+    end_date = df1['date'].max()
 
-    dff = dff[(dff['date']>=start_date) & (dff['date']<=end_date)]
+    df1 = df1[(df1['date']>=start_date) & (df1['date']<=end_date)]
     
-    dff = dff.query(f'continent == "{continent}"')
+    df1 = df1.query(f'continent == "{continent}"')
    
-    dff = dff.groupby('location').mean().sort_values(by='new_cases_per_million', ascending=False)[:10]
-    fig_pie = px.pie(dff, names=dff.index, values=dff['new_cases_per_million'],hole=0.7,color=dff.index, title='New cases per million')
+    df1 = df1.groupby('location').mean().sort_values(by='new_cases_per_million', ascending=False)[:10]
+    fig_pie = px.pie(df1, names=df1.index, values=df1['new_cases_per_million'],hole=0.7,color=df1.index, title='New cases per million')
     fig_pie.update_traces(hovertemplate=None, textposition='inside', textinfo='percent+label', rotation=45)
     fig_pie.update_layout(showlegend=False,
-                          plot_bgcolor='#202020', paper_bgcolor='#202020',
+                          plot_bgcolor='#000000', paper_bgcolor='#000000',
+                          title=dict(y=0.9, x=0.5, yanchor='top', xanchor='center'),
                           title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
                           font=dict(color='#8a8d93'),
                           hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
@@ -208,17 +200,17 @@ def update_area(continent):#, start_date, end_date):
 )
 def update_hist1(continent):#, start_date, end_date):
     
-    dff = data.copy()
+    df1 = data.copy()
     
-    start_date = dff['date'].min()
-    end_date = dff['date'].max()
+    start_date = df1['date'].min()
+    end_date = df1['date'].max()
 
-    dff = dff[(dff['date']>=start_date) & (dff['date']<=end_date)]
+    df1 = df1[(df1['date']>=start_date) & (df1['date']<=end_date)]
     
-    dff = dff.query(f'continent == "{continent}"')
+    df1 = df1.query(f'continent == "{continent}"')
 
 
-    fig_hist1 = px.histogram(data_frame = dff, y = 'location', x = 'people_fully_vaccinated_per_million', color = 'location',
+    fig_hist1 = px.histogram(data_frame = df1, y = 'location', x = df1['people_fully_vaccinated_per_million'].rolling(7).mean(), color = 'location',
              color_discrete_sequence=px.colors.qualitative.Plotly, 
              labels = {'location': 'Country', 'people_fully_vaccinated_per_million': 'Fully vaccinated per million'},
              title = 'Fully vaccinated per million')
@@ -227,12 +219,13 @@ def update_hist1(continent):#, start_date, end_date):
     fig_hist1.update_xaxes(visible=False)
 
     fig_hist1.update_layout(margin=dict(t=100, b=10, l=70, r=40),showlegend=False,
-                  hovermode="y unified", 
-                  yaxis_title=" ", 
-                  plot_bgcolor='#202020', paper_bgcolor='#202020',
-                  title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
-                  font=dict(color='#8a8d93'),
-                  hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
+                            hovermode="y unified", 
+                            yaxis_title=" ", 
+                            plot_bgcolor='#000000', paper_bgcolor='#000000',
+                            title=dict(y=0.9, x=0.5, yanchor='top', xanchor='center'),
+                            title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
+                            font=dict(color='#8a8d93'),
+                            hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
     
     return fig_hist1
 
@@ -245,36 +238,74 @@ def update_hist1(continent):#, start_date, end_date):
 )
 def update_hist2(continent):#, start_date, end_date):
     
-    dff = data.copy()
+    df2 = data.copy()
 
-    start_date = dff['date'].min()
-    end_date = dff['date'].max()
+    start_date = df2['date'].min()
+    end_date = df2['date'].max()
 
-    dff = dff[(dff['date']>=start_date) & (dff['date']<=end_date)]
+    df2 = df2[(df2['date']>=start_date) & (df2['date']<=end_date)]
     
-    dff = dff.query(f'continent == "{continent}"')
+    df2 = df2.query(f'continent == "{continent}"')
 
 
-    fig_hist2 = px.histogram(data_frame = dff, y = 'location', x = 'new_deaths_per_million', color = 'location',
-             color_discrete_sequence=px.colors.qualitative.Plotly, 
-             labels = {'location': 'Country', 'new_deaths_per_million': 'Deaths per million'},
-             title = 'Deaths per million', 
-             #template='plotly_dark'
-             )
+    fig_hist2 = px.histogram(data_frame = df2, y = 'location', x = 'new_deaths_per_million', color = 'location',
+                             color_discrete_sequence=px.colors.qualitative.Plotly, 
+                             labels = {'location': 'Country', 'new_deaths_per_million': 'Deaths per million'},
+                             title = 'Deaths per million', 
+                             #template='plotly_dark'
+                            )
 
     fig_hist2.update_yaxes(showgrid=False, ticksuffix=' ', showline=False, categoryorder='total ascending')
     fig_hist2.update_xaxes(visible=False)
 
     fig_hist2.update_layout(margin=dict(t=100, b=10, l=70, r=40),showlegend=False,
-                  hovermode="y unified", 
-                  yaxis_title=" ", 
-                  plot_bgcolor='#202020', paper_bgcolor='#202020',
-                  title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
-                  font=dict(color='#8a8d93'),
-                  hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
+                            hovermode="y unified", 
+                            yaxis_title=" ", 
+                            plot_bgcolor='#000000', paper_bgcolor='#000000',
+                            title=dict(y=0.9, x=0.5, yanchor='top', xanchor='center'),
+                            title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
+                            font=dict(color='#8a8d93'),
+                            hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
     
     return fig_hist2
 
+# Line Chart ***********************************************************
+@app.callback(
+    Output('line-plot','figure'),
+    Input('continent-dropdown','value'),
+    #Input('my-date-picker-start','date'),
+    #Input('my-date-picker-end','date'),
+)
+def update_line(continent):#, start_date, end_date):
+    
+    df3 = data.copy()
+
+    start_date = df3['date'].min()
+    end_date = df3['date'].max()
+
+    df3 = df3[(df3['date']>=start_date) & (df3['date']<=end_date)]
+    
+    df3 = df3.query(f'continent == "{continent}"')
+
+
+    fig_line = px.area(data_frame = df3, x = 'date', y = 'new_cases_per_million', color = 'location',
+                       color_discrete_sequence=px.colors.qualitative.Plotly, 
+                       labels = {'location': 'Country', 'new_cases_per_million': 'Cases per million'},
+                       title = 'Cases per million')
+
+    fig_line.update_yaxes(showgrid=False, ticksuffix=' ', showline=False, categoryorder='total ascending')
+    fig_line.update_xaxes(visible=False)
+
+    fig_line.update_layout(margin=dict(t=100, b=10, l=70, r=40),showlegend=True,
+                           hovermode="y unified", 
+                           yaxis_title=" ", 
+                           plot_bgcolor='#000000', paper_bgcolor='#000000',
+                           title=dict(y=0.9, x=0.5, yanchor='top', xanchor='center'),
+                           title_font=dict(size=25, color='#8a8d93', family="Lato, sans-serif"),
+                           font=dict(color='#8a8d93'),
+                           hoverlabel=dict(bgcolor="#c6ccd8", font_size=13, font_family="Lato, sans-serif"))
+    
+    return fig_line
 
 
 if __name__ =='__main__':
